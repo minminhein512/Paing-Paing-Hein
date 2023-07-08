f386db2e418e41cc461a0fe43d5bafd635b86993
minminhein512 <138702610+minminhein512@users.noreply.github.com>
2023-07-08 20:50:30 +0700

    Add files via upload

---
+++ ovpn-nginx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;32m-        VPS script MYANMAR      -\033[0m"
+echo -e "\033[0;32m-        Paing Paing Hein BY TAST_V1           -\033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+systemctl stop apache2
+fuser -k 80
+wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/upload-ovpn.sh
+echo "wait..."
+sudo apt install nginx -y
+cp /tmp/upload-ovpn.sh /usr/bin/
+chmod +x /usr/bin/upload-ovpn.sh
+echo ""
+echo "echo done just type -----------  upload-ovpn.sh ----------- to make ovpn files downloadable thru nginx"
+echo "exiting........"
+sleep 2
